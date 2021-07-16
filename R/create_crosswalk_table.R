36f2a7a6d18cae56605b7933dd37554ff9287590
Alec Trusty <a.e.trusty@gmail.com>
2021-07-16 15:40:08 -0700

    Wrote script to map ftype rasters to aa

--- R/create_crosswalk_table.R
+++ R/create_crosswalk_table.R
@@ -10,16 +10,34 @@ data_dir <- '/home/atrusty/github/ftype-to-assessment/data/'
 supersect_sf <- st_read(paste0(data_dir, 'CAR_supersections/CAR_supersections_coded.shp'))
 supersect_sf$ss_code <- as.character(supersect_sf$ss_code)
 
+# Read in forest type to common name and format as data table
+fortypcmn_json <- fromJSON(file = paste0(data_dir, 'fia_fortyp_to_common_name.json'))
+fortypcmn_dt <- fortypcmn_json %>% data.table()
+names(fortypcmn_dt) <- as.character("ftype_name")
+fortypcmn_dt$ftype_code <- lapply(seq_along(fortypcmn_json), function(i) names(fortypcmn_json)[[i]]) %>% unlist()
+
 # Read in species-forest type codes and format as a data table
 fortypcds_json <- fromJSON(file = paste0(data_dir, "arb_fortypcds.json")) # 348
 fortypcds_dt <- fortypcds_json %>% data.table()
-names(fortypcds_dt) <- "ftype_list"
+names(fortypcds_dt) <- as.character("ftype_list")
 fortypcds_dt$aa_code <- lapply(seq_along(fortypcds_json), function(i) names(fortypcds_json)[[i]]) %>% unlist()
 
 # Read in assessment area data 
 ## NOTE: We need to link our forest type data to an aa_code, then we can join to this table to get a common practice value
 aa_dt <- fread(paste0(data_dir,'2015_assessment_area_data_file.csv')) %>%
   .[, c("supersection", "assessment_area", "aa_code", "ss_code", "species", "site_class", "common_practice")]
+
+# Update assessment area name
+aa_dt$assessment_area <- lapply(aa_dt$assessment_area, function(x){
+  ss_name <- aa_dt[assessment_area == x,]$supersection[[1]]
+  aa_name <- aa_dt[assessment_area == x,]$assessment_area[[1]]
+  ss_words <- strsplit(ss_name, " ") %>% unlist()
+  aa_words <- strsplit(aa_name, " ") %>% unlist()
+
+  aa_only <- aa_words[!(aa_words %in% ss_words)]
+  paste0(aa_only, collapse = " ")
+}) %>% unlist()
+
 aa_uni_dt <- aa_dt[, list(
   ss_name= min(supersection),
   aa_name = min(assessment_area),
@@ -37,5 +55,29 @@ aa_uni_dt <- aa_uni_dt[!(aa_uni_dt$aa_code %in% sc_all),]
 # Join the forest type list to assessment data table
 aa_ftype_dt <- aa_uni_dt[fortypcds_dt, on = 'aa_code', c("ss_name", "ss_code", "ftype_list", "aa_name", "species", "aa_code", "cp_lo", "cp_hi")]
 
+# Break out the ftypes into individual rowsc
+aa_ftype_expanded_dt <- lapply(aa_ftype_dt$aa_code, function(x)  {
+  
+  dt <- aa_ftype_dt[aa_ftype_dt$aa_code == x, ]
+  
+  dt_ftype_list <- dt$ftype_list[[1]]
+
+  dt_expanded <- lapply(dt_ftype_list, function(y){
+    dt_exp_y <- dt
+    dt_exp_y$ftype_code <- as.character(y)
+    dt_exp_y
+  }) %>% rbindlist()
+
+  dt_expanded
+}) %>% rbindlist()
+
+# Join the forest type common name to the data table
+aa_ftype_expanded_dt <- fortypcmn_dt[aa_ftype_expanded_dt, on='ftype_code']
+aa_ftype_expanded_dt$ftype_name <- as.character(aa_ftype_expanded_dt$ftype_name)
+
+# Trim columns
+cols2keep <- c("ftype_code", "ftype_name", "aa_code", "aa_name", "ss_code", "ss_name", "cp_lo", "cp_hi", "species")
+aa_crosswalk_dt <- aa_ftype_expanded_dt[,cols2keep, with=FALSE]
+
 # Write out a copy of the forest type to assessment area crosswalk
-# fwrite(aa_ftype_dt, paste0(data_dir, "ftype_to_assessment_crosswalk.csv"))
+#fwrite(aa_crosswalk_dt, paste0(data_dir, "ftype_to_assessment_crosswalk.csv"))
---
+++ R/ftype_to_aa_crosswalk.R
@@ -0,0 +1,76 @@
+library(data.table)
+library(sf)
+library(rjson)
+library(dplyr)
+library(raster)
+library(fasterize)
+library(parallel)
+
+# Set paths
+raster_dir <- '/home/atrusty/NewForests/Projects/Dendro/dendroBasemap/'
+data_dir <- '/home/atrusty/github/ftype-to-assessment/data/'
+
+# Read in forest type to assessment crosswalk
+aa_ftype_dt <- fread(paste0(data_dir, "ftype_to_assessment_crosswalk.csv"))
+
+# Read in supersection shapefile
+supersect_sf <- st_read(paste0(data_dir, 'CAR_supersections/CAR_supersections_coded.shp'))
+supersect_sf$ss_code <- as.character(supersect_sf$ss_code)
+
+# Read in forest type and supersections raster
+ss_raster <- raster(paste0(raster_dir, 'CONUS-supersections-240m.tif'))
+ftype_raster <- raster(paste0(raster_dir, 'CONUS-forest_type-240m.tif'))
+
+# Stack the rasters
+conus_stack <- stack(ss_raster, ftype_raster)
+
+# Create a lookup and reclassification matrix to go from forest type to assessment code
+aa_lookup_dt <- aa_ftype_dt[,c("ftype_code", "ss_code", "aa_code")]
+reclass_matrix <- aa_lookup_dt[,c("ftype_code", "aa_code")]
+
+# Get a list of unique supersection values - NOTE - the shapefile does not have supersection code 62: Ouachita Mixed Forest
+unique_ss <- aa_lookup_dt$ss_code[!aa_lookup_dt$ss_code==62] %>% unique %>% .[!is.na(.)]
+
+# Set up a clusters to compute in parallel
+cl <- makeCluster(detectCores())
+aa_rast_list <- parLapply(cl, unique_ss, function(x, aa_lookup_dt, supersect_sf, ftype_raster, reclass_matrix){
+  library(sf)
+  library(magrittr)
+  library(raster)
+  library(data.table)
+  
+  # Grab codes
+  ftype_to_aa_dt <- aa_lookup_dt[ss_code==x,]
+  # ftypes <- ftype_to_aa_dt$ftype_code %>% unique
+  # aa_codes <- ftype_to_aa_dt$aa_code %>% unique
+  
+  # Read in supersection sf and mask rasters to the boundaries
+  ss_subset_sf <- supersect_sf[supersect_sf$ss_code==x,] %>% st_transform(crs(ftype_raster))
+  # ss_sub_rst <- crop(ss_raster, extent(ss_subset_sf))
+  ftype_sub_rst <- crop(ftype_raster, extent(ss_subset_sf))
+  # ss_masked <- mask(ss_sub_rst, ss_subset_sf)
+  # names(ss_masked) <- "SS"
+  ftype_masked <- mask(ftype_sub_rst, ss_subset_sf)
+  names(ftype_masked) <- "FTYPE"
+  
+  reclassify(ftype_masked, reclass_matrix)
+}, aa_lookup_dt, supersect_sf, ftype_raster, reclass_matrix)
+stopCluster(cl)
+
+# Define the mosaic function and mosaic the raster list
+aa_rast_list$fun <- min
+aa_mosaic <- do.call(mosaic, aa_rast_list)
+
+# Write the raster to disk
+# writeRaster(aa_mosaic, paste0(raster_dir, "CONUS-assessment-areas-240m.tif"))
+
+# Using the assessment area mosaic, reclassify it to common practice values
+aa_to_cp_lo_rcl <- aa_ftype_dt[,c("aa_code", "cp_lo")]
+aa_to_cp_hi_rcl <- aa_ftype_dt[,c("aa_code", "cp_hi")]
+
+cp_lo_rast <- reclassify(aa_mosaic, aa_to_cp_lo_rcl)
+cp_hi_rast <- reclassify(aa_mosaic, aa_to_cp_hi_rcl)
+
+# Write each to disk
+# writeRaster(cp_lo_rast, paste0(raster_dir, "CONUS-common-practice-low-240m.tif"))
+# writeRaster(cp_hi_rast, paste0(raster_dir, "CONUS-common-practice-hi-240m.tif"))
